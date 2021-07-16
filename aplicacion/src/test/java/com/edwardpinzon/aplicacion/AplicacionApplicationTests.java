fb42954c95c8e149fc3bc6c36a8cded88fadbd1c
edward.pinzon@gtd.com.co <EMPUMATE123>
2021-07-16 14:47:12 -0500

    Commit Inicial con eclipse

---
+++ aplicacion/src/test/java/com/edwardpinzon/aplicacion/AplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardpinzon.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
